913239f49931eb0f668a9751802fb33353b253d9
pkrueger <philip.krueger@caci.com>
2019-05-06 15:40:40 -0700

    finished

--- MouseDetectionApp/Form1.Designer.cs
+++ MouseDetectionApp/Form1.Designer.cs
@@ -28,13 +28,49 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.panel = new System.Windows.Forms.Panel();
+      this.eventLabel = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // panel
+      // 
+      this.panel.Location = new System.Drawing.Point(12, 12);
+      this.panel.Name = "panel";
+      this.panel.Size = new System.Drawing.Size(776, 406);
+      this.panel.TabIndex = 0;
+      this.panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.onClick);
+      this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.onMouseDown);
+      this.panel.MouseEnter += new System.EventHandler(this.onMouseEnter);
+      this.panel.MouseLeave += new System.EventHandler(this.onMouseLeave);
+      this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.onMouseMove);
+      this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.onMouseUp);
+      // 
+      // eventLabel
+      // 
+      this.eventLabel.AutoSize = true;
+      this.eventLabel.Location = new System.Drawing.Point(21, 427);
+      this.eventLabel.Name = "eventLabel";
+      this.eventLabel.Size = new System.Drawing.Size(0, 13);
+      this.eventLabel.TabIndex = 1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.Controls.Add(this.eventLabel);
+      this.Controls.Add(this.panel);
+      this.Name = "Form1";
+      this.Text = "Mouse Detection";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Panel panel;
+    private System.Windows.Forms.Label eventLabel;
   }
 }
 
--- MouseDetectionApp/Form1.cs
+++ MouseDetectionApp/Form1.cs
@@ -12,9 +12,64 @@ namespace MouseDetectionApp
 {
   public partial class Form1 : Form
   {
+    private Point mousePosition = new Point(0, 0);
+    private bool dragging = false;
+    private bool mouseDown = false;
     public Form1()
     {
       InitializeComponent();
+      panel.BackColor = Color.White;
+      eventLabel.Text = "Mouse outside panel";
+    }
+
+    private void onClick(object sender, MouseEventArgs e)
+    {
+
+    }
+
+    private void onMouseDown(object sender, MouseEventArgs e)
+    {
+      if ((Panel)sender == panel)
+      {
+        mouseDown = true;
+        eventLabel.Text = $"Pressed at [{e.X}, {e.Y}]";
+      }
+    }
+
+    private void onMouseEnter(object sender, EventArgs e)
+    {
+      panel.BackColor = Color.LimeGreen;
+      eventLabel.Text = "Mouse inside panel";
+    }
+
+    private void onMouseLeave(object sender, EventArgs e)
+    {
+      panel.BackColor = Color.White;
+      eventLabel.Text = "Mouse outside panel";
+    }
+
+    private void onMouseMove(object sender, MouseEventArgs e)
+    {
+      if ((Panel)sender == panel &&
+        e.X != mousePosition.X &&
+        e.Y != mousePosition.Y
+        )
+      {
+        mousePosition = new Point(e.X, e.Y);
+        if (mouseDown)
+          eventLabel.Text = $"Dragged at [{e.X}, {e.Y}]";
+        else
+          eventLabel.Text = $"Moved at [{e.X}, {e.Y}]";
+      }
+    }
+
+    private void onMouseUp(object sender, MouseEventArgs e)
+    {
+      if ((Panel)sender == panel)
+      {
+        mouseDown = false;
+        eventLabel.Text = $"Released at [{e.X}, {e.Y}]";
+      }
     }
   }
 }
